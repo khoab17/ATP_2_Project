bda3c7d204d433f9aa6eda73df349efc1d49e0bf
Syed Abdullah <syed_khoab@outlook.com>
2021-07-03 19:54:56 +0600

    Admin Registration

---
+++ ECS/ECS/Controllers/AdminController.cs
@@ -0,0 +1,39 @@
+﻿using ECS.Models;
+using ECS.Models.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECS.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        ECSEntities context = new ECSEntities();
+
+        public ActionResult Index()
+        {
+            var users = context.Users.ToList();
+            return View(users);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(UserCredential userCredential)
+        {
+            //context.Users.Add(admin);
+            //context.SaveChanges();
+            context.Users.Add(userCredential.User);
+            context.Credentials.Add(userCredential.Credential);
+            context.SaveChanges();
+            
+            return RedirectToAction("Index","Admin");
+        }
+    }
+}
---
+++ ECS/ECS/Models/ViewModel/UserCredential.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ECS.Models.ViewModel
+{
+    public class UserCredential
+    {
+        public User User { get; set; }
+        public Credential Credential { get; set; }
+    }
+}
